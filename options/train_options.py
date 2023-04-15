5de6a6bd47838fc6d664d8f94c57e013fc2c9f47
hudididdi <113669594+hudididdi@users.noreply.github.com>
2023-04-15 18:27:04 +0800

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -11,7 +11,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--display_env', type=str, default='main', help='visdom display environment name (default is "main")')
         parser.add_argument('--display_server', type=str, default="http://localhost", help='visdom server of the web display')
         parser.add_argument('--update_html_freq', type=int, default=4000, help='frequency of saving training results to html')
-        parser.add_argument('--print_freq', type=int, default=1024, help='frequency of showing training results on console')
+        parser.add_argument('--print_freq', type=int, default=2048, help='frequency of showing training results on console')
         parser.add_argument('--save_latest_freq', type=int, default=10000, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
         parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
@@ -27,7 +27,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         parser.add_argument('--lr_decay_iters', type=int, default=100, help='multiply by a gamma every lr_decay_iters iterations')
         # lambda parameters
-        parser.add_argument('--lambda_L1', type=float, default=10.0, help='weight for |B-G(A, E(B))|')
+        parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for |B-G(A, E(B))|')
         parser.add_argument('--lambda_GAN', type=float, default=1.0, help='weight on D loss. D(G(A, E(B)))')
         parser.add_argument('--lambda_GAN2', type=float, default=1.0, help='weight on D2 loss, D(G(A, random_z))')
         parser.add_argument('--lambda_z', type=float, default=0., help='weight for ||E(G(random_z)) - random_z||')
